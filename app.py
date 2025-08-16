29ee134c2637b1464f2c4bc1c74e46459837f5ed
Mohamed <mohamedboualamallah@icloud.com>
2025-08-16 04:55:42 +0300

    screen and test rectangle

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-import tkinter
+import tkinter as tk
 
-root = tkinter.Tk()
+root = tk.Tk()
+root.title("Rectangle Example")
+
+screenwidth, screenheight = 800, 600
+
+canvas = tk.Canvas(root, width=screenwidth, height=screenheight, bg="lightgray")
+canvas.pack()
 
 
+canvas.create_rectangle(0, 0, screenwidth/2, screenheight, fill="blue")
 
 root.mainloop()
